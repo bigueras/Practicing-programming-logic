87d2a24115fcc6101ac9ace6061fdf4d0a2db250
bigueras <lucascarmosoaresgmail.com>
2025-04-30 02:21:01 -0300

    tabuada

---
+++ Tabuada.java
@@ -0,0 +1,54 @@
+
+package seção3;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Digite o numero:" );
+        int numero = scanner.nextInt();
+        System.out.println(numero * 1);
+        System.out.println(numero * 2);
+        System.out.println(numero * 3);
+        System.out.println(numero * 4);
+        System.out.println(numero * 5);
+        System.out.println(numero * 6);
+        System.out.println(numero * 7);
+        System.out.println(numero * 8);
+        System.out.println(numero * 9);
+        System.out.println(numero * 10);
+        
+
+        System.out.println("Digite um numero");
+        
+        double numeroquebrado =scanner.nextDouble();
+        System.out.println(numeroquebrado * 1);
+        System.out.println(numeroquebrado * 2);
+        System.out.println(numeroquebrado * 3);
+        System.out.println(numeroquebrado * 4);
+        System.out.println(numeroquebrado * 5);
+        System.out.println(numeroquebrado * 6);
+        System.out.println(numeroquebrado * 7);
+        System.out.println(numeroquebrado * 8);
+        System.out.println(numeroquebrado * 9);
+        System.out.println(numeroquebrado * 10);
+        scanner.close();
+
+        
+        
+       
+
+
+        
+
+
+
+
+
+
+        
+    }
+    
+}
