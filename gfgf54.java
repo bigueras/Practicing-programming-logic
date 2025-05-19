411596ae1ad22e64fdc1a9c944707ec64e0e5f5a
Lucas Soares <121404389+bigueras@users.noreply.github.com>
2025-05-19 18:45:17 -0300

    Add files via upload

---
+++ gfgf54.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex02 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 12;
+        
+    }
+    public static int soma (int a, int b) {
+        return a+b;
+        
+    }
+}
