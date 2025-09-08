e93863a90cac1ef3ed6971d29e0ee479df2b4129
Lucas Soares <121404389+bigueras@users.noreply.github.com>
2025-09-07 23:57:33 -0300

    Exercícios

---
+++ ex81.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex81 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int numero = leitor.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par");
+            
+        } else {
+            System.out.println("O número é impar");
+        }
+
+        leitor.close();
+    }
+}
