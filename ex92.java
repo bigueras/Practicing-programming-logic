ebad271d469333fd3ac8fce1c9a9bb32be0c5484
Lucas Soares <121404389+bigueras@users.noreply.github.com>
2025-10-01 23:31:16 -0300

    Exercícios

---
+++ ex92.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ex92 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+
+        int numero = leitor.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par");
+            
+        } else {
+            System.out.println("O número é ímpar");
+        }
+        leitor.close();
+    }
+}
