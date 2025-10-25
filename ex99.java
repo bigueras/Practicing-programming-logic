734624bdabe48b746cd65be1e7985d632f30d8e4
Lucas Soares <121404389+bigueras@users.noreply.github.com>
2025-10-25 00:56:57 -0300

    Exercícios

---
+++ ex99.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ex99 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+    
+        int numero = leitor.nextInt();
+         
+        if (numero > 0) {
+            System.out.println(" O número é positivo");
+            
+        } else {
+            System.out.println("O número é negativo");
+        }
+
+        leitor.close();
+
+
+
+    }
+
+}
